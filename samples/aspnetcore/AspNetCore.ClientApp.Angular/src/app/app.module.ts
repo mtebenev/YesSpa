8ec39e6a1ecfd654fb741ad93b1da5a2280c8a4a
Maxim Tebenev <mtebenev@gmail.com>
2018-11-10 15:35:55 +0300

    Added simple routing in angular sample

--- samples/aspnetcore/AspNetCore.ClientApp.Angular/src/app/app.module.ts
+++ samples/aspnetcore/AspNetCore.ClientApp.Angular/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PrimaryComponent } from './primary.component';
+import { SecondaryComponent } from './secondary.component';
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  { path: 'primary', component: PrimaryComponent },
+  { path: 'secondary', component: SecondaryComponent },
+  {
+    path: '',
+    redirectTo: '/primary',
+    pathMatch: 'full'
+  }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimaryComponent,
+    SecondaryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ samples/aspnetcore/AspNetCore.ClientApp.Angular/src/app/primary.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-component',
+  templateUrl: './primary.component.html'
+})
+export class PrimaryComponent {
+}
